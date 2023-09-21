948e2690ed54e3e3ab1cbd48cc38e2b0ba147b8a
User <User@DESKTOP-D3727QP>
2023-09-21 22:23:30 +0300

    Добавьте файлы проекта.

---
+++ Project1/FileName.c
@@ -0,0 +1,20 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("     ___           /|");
+	puts("     | |          / |");
+	puts("     | |         /  |");
+	puts("     | |        /   |");
+	puts("  ___|_|_______/____|");
+	puts(" /                  |");
+	puts("|                   |");
+	puts("|___________________|");
+	puts("     | |");
+	puts("     | |");
+	puts("     | |");
+	puts("     |_|");
+	puts("яюлнкерхй");
+	getchar();
+}
